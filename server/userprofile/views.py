2c5b6997b2ea56f53bd6c48389317e2d8b506b68
lijm1358 <lijm1358@gmail.com>
2022-08-26 17:02:21 +0900

    feat: add django app userprofile

---
+++ server/userprofile/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .serializers import ProfileSerializer
+from .models import UserProfile
+
+# Create your views here.
+class ProfileViewSet(viewsets.ViewSet):
+    def list(self, request, **kwargs):
+        targetId = request.GET["targetId"]
+        if targetId is None:
+            return Response("Query \"targetId\" is required to get user profile.", status=400)
+        
+        try:
+            profileObject = UserProfile.objects.get(userid=targetId)
+            serializer = ProfileSerializer(profileObject)
+            if serializer.is_valid():
+                return Response(serializer.data, status=200)
+        except ObjectDoesNotExist:
+            return Response(f'Can\'t find profile information with user id {targetId}.')

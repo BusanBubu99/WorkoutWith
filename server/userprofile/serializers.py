2c6beedcf0925f9727437f327ebc7cf7315a9d59
lijm1358 <lijm1358@gmail.com>
2022-08-26 17:03:04 +0900

    feat: add serializer for user profile

---
+++ server/userprofile/serializers.py
@@ -0,0 +1,10 @@
+from userprofile.models import UserProfile
+from rest_framework import serializers
+
+
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    profilePic = serializers.ImageField(use_url=True)
+
+    class Meta:
+        model = UserProfile
+        fields = ('userid', 'name', 'profilePic', 'tags')

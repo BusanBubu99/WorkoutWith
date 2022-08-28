d127cb946204ee27a006a28cf8eb86e05125b7a9
lijm1358 <lijm1358@gmail.com>
2022-08-28 23:54:45 +0900

    feat: add default image for profilepic

--- server/userprofile/models.py
+++ server/userprofile/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class UserProfile(models.Model):
     userid = models.CharField(max_length=15, unique=True)
     name = models.CharField(max_length=10)
-    profilePic = models.ImageField()
+    profilePic = models.ImageField(upload_to = "profilepic/", null=True, default='media/default_profileimage.png')
     tags = models.CharField(max_length=10)

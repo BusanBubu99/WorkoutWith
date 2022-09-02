adc43f8c23b73292ac828de34e5a18640d9b004c
lijm1358 <lijm1358@gmail.com>
2022-09-02 22:29:33 +0900

    feat: add some auth classes

--- server/WorkoutWithServer/settings.py
+++ server/WorkoutWithServer/settings.py
@@ -65,6 +65,9 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 

98985a0dbc660eb3cebed575ace9767ea9712902
lijm1358 <lijm1358@gmail.com>
2022-09-02 18:21:40 +0900

    fix: change authentication classes

--- server/WorkoutWithServer/settings.py
+++ server/WorkoutWithServer/settings.py
@@ -64,10 +64,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        #'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
 

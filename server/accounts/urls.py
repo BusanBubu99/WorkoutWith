76abb7e6a7ca0ccd7d5f7db6ebaeccbf5f8428a0
lijm1358 <lijm1358@gmail.com>
2022-09-03 20:51:10 +0900

    feat: update urls for email verification

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
+from dj_rest_auth.registration.views import VerifyEmailView, RegisterView
+
+from dj_rest_auth.views import (
+    LoginView, LogoutView, PasswordChangeView,
+    PasswordResetView, PasswordResetConfirmView
+)
+from accounts.views import ConfirmEmailView
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
+    path('allauth/', include('allauth.urls')),
+    path('accounts/allauth/', include('allauth.urls')),
+    re_path(r'^account-confirm-email/$', VerifyEmailView.as_view(), name='account_email_verification_sent'),
+    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email')
 ]

a4dbc0ccd6f8fb468cc9c6a9e9f5c573f838a8ea
lijm1358 <lijm1358@gmail.com>
2022-09-08 18:51:47 +0900

    feat: add password reset feature

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -8,8 +8,10 @@ from dj_rest_auth.views import (
 from accounts.views import ConfirmEmailView
 
 urlpatterns = [
-    path('', include('dj_rest_auth.urls')),
+    path('password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password/reset', PasswordResetView.as_view(), name="rest_password_reset"),
     path('registration/', include('dj_rest_auth.registration.urls')),
+    path('', include('dj_rest_auth.urls')),
     path('allauth/', include('allauth.urls')),
     path('accounts/allauth/', include('allauth.urls')),
     re_path(r'^account-confirm-email/$', VerifyEmailView.as_view(), name='account_email_verification_sent'),

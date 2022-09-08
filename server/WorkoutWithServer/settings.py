a91774d1b2dc294c88d7695aa16edaf710803c59
JongMok Lee <lijm1358@gmail.com>
2022-09-08 18:57:50 +0900

    fix : remove duplicate in settings.py

--- server/WorkoutWithServer/settings.py
+++ server/WorkoutWithServer/settings.py
@@ -153,9 +153,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
 # Email Verification
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 

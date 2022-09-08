c253dc4ed888ab2f2d6f09ed3ccc91a90fa5a2be
JongMok Lee <lijm1358@gmail.com>
2022-09-08 18:57:04 +0900

    fix: remove duplicate in settings.py

--- server/WorkoutWithServer/settings.py
+++ server/WorkoutWithServer/settings.py
@@ -70,7 +70,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
